798ae107789f1a775fdd24d52ecd5b3f55517402
Shane Schmaltz <stschmaltz@gmail.com>
2019-01-21 21:07:46 -0700

    cleanup

--- marketing-demo-site/src/modules/footer/Footer.js
+++ marketing-demo-site/src/modules/footer/Footer.js
@@ -5,9 +5,7 @@ import './Footer.css'
 import sizeMe from 'react-sizeme'
 
 const Footer = ({ size }) => {
-  console.log(size)
   const { width } = size
-  console.log(width)
 
   return (
     <div className="footer__main-container">
